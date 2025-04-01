ef1a82d47f15d984492e2f9f7cbf13db64b80dac
Adrien Carton de Wiart <A.J.A.CartondeWiart@student.tudelft.nl>
2025-04-01 13:50:06 +0200

    comment for clarity

--- agents/team 16 agent/utils/wrapper.py
+++ agents/team 16 agent/utils/wrapper.py
@@ -12,4 +12,5 @@ def get_opponent_data(name):
     #for now returns default opponent
     henri = Oponent()
     #also can be henri = Oponent(1, .5, [1,2,2,,1])
+    #you can also do henri.result [or whatever you name the return of this function] and compute on that
     return henri
