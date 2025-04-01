9f8aa4d4f2c04bd7ac3ed9b9dcd806ae18a0e91b
Adrien Carton de Wiart <A.J.A.CartondeWiart@student.tudelft.nl>
2025-04-01 13:43:15 +0200

    create template for object

---
+++ agents/team 16 agent/utils/wrapper.py
@@ -0,0 +1,15 @@
+# python objects to store oponent information
+
+class Oponent:
+    def __init__(self, result = 0, finalUtility = 0, offerVariance = []):
+        self.result = result
+        self.finalUtility = finalUtility
+        self.offerVariance = offerVariance
+
+
+
+def get_opponent_data(name):
+    #for now returns default opponent
+    henri = Oponent()
+    #also can be henri = Oponent(1, .5, [1,2,2,,1])
+    return henri
